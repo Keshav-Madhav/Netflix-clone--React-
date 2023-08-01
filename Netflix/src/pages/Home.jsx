d148a0fb7760190e8c5cbf2be4a5cfd4e20acb47
Keshav <keshav2552003@gmail.com>
2023-08-01 22:43:46 +0530

    Added row component with approriate fetch of movie

--- Netflix/src/pages/Home.jsx
+++ Netflix/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Index from '../components/Index';
+import Row from '../components/Row';
+import requests from '../Requests';
 
 const Home = () => {
     return (
-        <div><Index/></div>
+        <>
+            <Index/>
+            <Row title='Up Coming' fetchURL={requests.requestUpcoming} />
+            <Row title='Popular' fetchURL={requests.requestPopular} />
+            <Row title='Trending' fetchURL={requests.requestTrending} />
+            <Row title='Top Rated' fetchURL={requests.requestTopRated} />
+            <Row title='Horror' fetchURL={requests.requestHorror} />
+        </>
     )
 }
 
