eb64b560384e9ba6002d1bad2d6a7bbb74ebde37
Keshav <keshav2552003@gmail.com>
2023-08-01 17:35:10 +0530

    Banner completed (index component)

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   
   return (
+    <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
